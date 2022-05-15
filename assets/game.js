1272f98a3ed94953e5f0312cf1d02cfa66c552eb
Your Name <drjpolanco25@yahoo.com>
2022-05-14 21:03:43 -0700

    random health and attack values

--- assets/game.js
+++ assets/game.js
@@ -22,15 +22,14 @@ var fight = function(enemyName) {
       if (confirmSkip) {
         window.alert(playerName + ' has decided to skip this fight. Goodbye!');
         // subtract money from playerMoney for skipping
-        playerMoney = Math.max(0, playerMoney - 10);
+        playerMoney =Math.max(0, playerMoney - 10);
         console.log("playerMoney", playerMoney)
         break;
       }
     }
 
     // remove enemy's health by subtracting the amount set in the playerAttack variable
-    var damage = randomNumber(playerAttack -3, playerAttack);
-    enemyHealth = Math.max(0, enemyHealth- damage);
+    enemyHealth = Math.max(0, enemyHealth - playerAttack);
     console.log(
       playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
     );
@@ -49,8 +48,7 @@ var fight = function(enemyName) {
     }
 
     // remove players's health by subtracting the amount set in the enemyAttack variable
-    var damage= randomNumber(enemyAttack - 3, enemyAttack);
-    playerHealth= Math.max(0, playerHealth- damage);
+    playerHealth = Math.max(playerHealth - enemyAttack);
     console.log(
       enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
     );
@@ -84,7 +82,8 @@ var startGame = function() {
       var pickedEnemyName = enemyNames[i];
 
       // reset enemyHealth before starting new fight
-      enemyHealth = randomNumber(40,60);
+      enemyHealth = 50;
+
       // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
       fight(pickedEnemyName);
 
@@ -179,10 +178,6 @@ var shop = function() {
       shop();
       break;
   }
-};
-var randomNumber = function () {
-  var value= Math.floor(Math.random() *(max -min +1) + min);
-return value;
 };
 
 // start first game when page loads
